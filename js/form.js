24d4672927429e4260bde39748320ee83230388b
Natela <n.kerd@mail.ru>
2021-10-27 23:06:14 +0300

    Правда или действие (часть 1)

--- js/form.js
+++ js/form.js
@@ -10,4 +10,57 @@ const switchState = (boolean) => {
   });
 };
 
+
+const MIN_TITLE_LENGTH = 30;
+const MAX_TITLE_LENGTH = 100;
+const title = document.querySelector('#title');
+
+title.addEventListener('input', () => {
+  const titleLength = title.value.length;
+
+  if (titleLength < MIN_TITLE_LENGTH) {
+    title.setCustomValidity(`Ещё ${MIN_TITLE_LENGTH - titleLength} симв.`);
+  } else if (titleLength > MAX_TITLE_LENGTH) {
+    title.setCustomValidity(`Удалите лишние ${  titleLength - MAX_TITLE_LENGTH } симв.`);
+  } else {
+    title.setCustomValidity('');
+  }
+  title.reportValidity();
+});
+
+const roomNumber = document.querySelector('#room_number');
+const gestsNumber = document.querySelector('#capacity');
+const gestOptions = gestsNumber.querySelectorAll('option');
+roomNumber.addEventListener('change', () => {
+  if (roomNumber.value === '3') {
+    gestOptions.forEach((option) => {
+      option.disabled = false;
+      if (option.value === '0') {
+        option.disabled = true;
+      }
+    });
+  } else if (roomNumber.value === '2') {
+    gestOptions.forEach((option) => {
+      option.disabled = false;
+      if (option.value === '0' || option.value === '3') {
+        option.disabled = true;
+      }
+    });
+  } else if (roomNumber.value === '1') {
+    gestOptions.forEach((option) => {
+      option.disabled = false;
+      if (option.value === '3' || option.value === '2' || option.value === '0') {
+        option.disabled = true;
+      }
+    });
+  } else if (roomNumber.value === '100') {
+    gestOptions.forEach((option) => {
+      option.disabled = false;
+      if (option.value === '3' || option.value === '2' || option.value === '1') {
+        option.disabled = true;
+      }
+    });
+  }
+});
+
 export {switchState};
