6aed49b9cad4a1dd00ace2f0256026653d8398b5
Natela <n.kerd@mail.ru>
2021-10-31 15:46:17 +0300

    Правда или действие (часть 2)

--- js/form.js
+++ js/form.js
@@ -1,14 +1,21 @@
+const MIN_TITLE_LENGTH = 30;
 const ad = document.querySelector('.ad-form');
 const controls = document.querySelectorAll('.ad-form-header, .ad-form__element, .map__filter');
 const mapFilters = document.querySelector('.map__filters');
 const checkIn = document.querySelector('#timein');
 const checkOut = document.querySelector('#timeout');
-const MIN_TITLE_LENGTH = 30;
 const title = document.querySelector('#title');
 const roomNumber = document.querySelector('#room_number');
 const guestsNumber = document.querySelector('#capacity');
 const price = document.querySelector('#price');
 const type = document.querySelector('#type');
+const TypePrice = {
+  bungalow: 0,
+  flat: 1000,
+  hotel: 3000,
+  house: 5000,
+  palace: 10000,
+};
 
 const switchState = (boolean) => {
   ad.classList.toggle('ad-form--disabled', boolean);
@@ -45,16 +52,9 @@ roomNumber.addEventListener('change', () => {
   validateRoomsCapacity();
 });
 
-const typePrice = {
-  bungalow: 0,
-  flat: 1000,
-  hotel: 3000,
-  house: 5000,
-  palace: 10000,
-};
 const getPrice = () => {
-  price.placeholder = typePrice[type.value];
-  price.min = typePrice[type.value];
+  price.placeholder = TypePrice[type.value];
+  price.min = TypePrice[type.value];
   price.reportValidity();
 };
 type.addEventListener('change', () => {
