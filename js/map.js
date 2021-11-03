813a001191109763c0fc3ee7a3397d37f4e9c132
Natela <n.kerd@mail.ru>
2021-11-03 22:34:14 +0300

    Помощь друга

--- js/map.js
+++ js/map.js
@@ -1,13 +1,30 @@
 import {switchState} from './form.js';
 import {createAnnouncement} from './data.js';
 import {render} from './announcement.js';
+const TOKYO_COORDINATES = {
+  lat: 35.6895,
+  lng: 139.692,
+};
+const PIN_SIZES = {
+  pinIcon: 40,
+  mainPinIcon: 52,
+};
+const PIN_ANCHORS = {
+  pinIcon: [20, 40],
+  mainPinIcon: [26, 52],
+};
+const SCALE = 10;
+const DIGITS = 5;
 
 const address = document.querySelector('#address');
 const map = L.map('map-canvas')
+  .on('load', () => {
+    switchState(false);
+  })
   .setView({
-    lat: 35.6895,
-    lng: 139.692,
-  }, 10);
+    lat: TOKYO_COORDINATES.lat,
+    lng: TOKYO_COORDINATES.lng,
+  }, SCALE);
 
 L.tileLayer(
   'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
@@ -16,18 +33,17 @@ L.tileLayer(
   },
 ).addTo(map);
 
-switchState(false);
 
 const mainPinIcon = L.icon({
   iconUrl: 'img/main-pin.svg',
-  iconSize: [52, 52],
-  iconAnchor: [26, 52],
+  iconSize: PIN_SIZES.mainPinIcon,
+  iconAnchor: PIN_ANCHORS.mainPinIcon,
 });
 
 const mainPinMarker = L.marker(
   {
-    lat: 35.6895,
-    lng: 139.692,
+    lat: TOKYO_COORDINATES.lat,
+    lng: TOKYO_COORDINATES.lng,
   },
   {
     draggable: true,
@@ -38,25 +54,24 @@ mainPinMarker.addTo(map);
 
 const pinIcon = L.icon({
   iconUrl: 'img/pin.svg',
-  iconSize: [40, 40],
-  iconAnchor: [20, 40],
+  iconSize: PIN_SIZES.pinIcon,
+  iconAnchor: PIN_ANCHORS.pinIcon,
 });
 
-address.value = `${mainPinMarker.getLatLng().lat.toFixed(5)}, ${mainPinMarker.getLatLng().lng.toFixed(5)}`;
+address.value = `${mainPinMarker.getLatLng().lat.toFixed(DIGITS)}, ${mainPinMarker.getLatLng().lng.toFixed(DIGITS)}`;
 
 mainPinMarker.on('moveend', (evt) => {
   address.value = `${evt.target.getLatLng().lat.toFixed(5)}, ${evt.target.getLatLng().lng.toFixed(5)}`;
 });
 
-
-for(let i = 1; i <= 11; i++) {
-  const createCustomPopup = () => render(createAnnouncement(i));
-  const lat = createAnnouncement().location.lat;
-  const lng = createAnnouncement().location.lng;
+const createPinMarker = (i) => {
+  const announcement = createAnnouncement(i);
+  const createCustomPopup = () => render(announcement);
+  const latLng = announcement.location;
   const pinMarker = L.marker(
     {
-      lat: lat,
-      lng: lng,
+      lat: latLng.lat,
+      lng: latLng.lng,
     },
     {
       icon: pinIcon,
@@ -64,6 +79,9 @@ for(let i = 1; i <= 11; i++) {
   );
   pinMarker.addTo(map)
     .bindPopup(createCustomPopup());
+};
+for(let i = 1; i <= 11; i++) {
+  createPinMarker(i);
 }
 
 export {map};
